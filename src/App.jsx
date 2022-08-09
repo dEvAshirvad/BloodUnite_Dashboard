e9e96d340c027724153bf2057fb30f6bd37ba81a
Ashirvad Satapathy <ashirvad.satapathy01@gmail.com>
2022-08-09 15:31:52 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Footer from './comps/Footer/Footer'
+import Header from './comps/Header/Header'
+
+function App() {
+
+  return (
+    <body>
+        <Header/>
+          <h1>Hello Jicky</h1>
+        <Footer/>
+    </body>
+  )
+}
+
+export default App
